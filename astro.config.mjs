73f8c43a80e45853bbdfcc456c95fe036eb783f4
osama59 <100736768+osama59@users.noreply.github.com>
2025-10-18 17:09:22 +0300

    Adding the visual Identity

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,20 +6,19 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			
+			title: 'Mediato',
+			customCss: [
+				// Relative path to your custom CSS file
+				'./src/styles/custom.css',
+			],
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/Osama59' }],
 			sidebar: [
 				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
+					label: 'المدخل لفن الوسائط المتعددة',
 					autogenerate: { directory: 'reference' },
 				},
+				
 			],
 		}),
 	],
